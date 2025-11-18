a0fe16e7ec2bd5fa26eec21b7d1d3d771ff1123c
Madhvesha Patil <madhveshapatil@gmail.com>
2025-11-18 15:03:49 +0530

    sixth

--- Controller/userController.js
+++ Controller/userController.js
@@ -7,4 +7,28 @@ exports.registerNewUser=async(req,res)=>{
     .json({success:true,message:"User Registered Successfully",payload})
 }
 
-
+exports.loginUser=async(req,res)=>{
+    const {email,password}=req.body
+    if(!email || !password){
+        res
+        .status(400)
+        .json({success:false,message:"Email and Password are mandatory"})
+    }
+    else{
+        const user=await userSchema.findOne({email}).select("+password")
+        if(!user){
+            res.status(400)
+            .json({success:false,message:"No User Found"})
+        }
+        else{
+            let isMatch=await user.comparePasswords(password)
+            if(isMatch){
+                res.status(200).json({success:false,message:"Login Successful"})
+            }
+            else{
+                res.status(400)
+                .json({success:false,message:"Invalid Password"})
+            }
+        }
+    }
+}
--- Schema/UserSchema.js
+++ Schema/UserSchema.js
@@ -1,17 +1,33 @@
 const mongoose=require('mongoose')
 const {Schema}=require('mongoose')
+const bcrypt =require('bcrypt')
 
 const userSchema= new Schema({
-    username:{
+    email:{
         type: String,
-        required: true
+        required: [true,"Email Should be String"],
+        unique:true
+    },username:{
+        type: String,
+        required: [true,"Username Should be String"]
     },
     password:{
         type:String,
-        required:true
+        required:[true,"password Should be String"],
+        minLength:6,
+        maxLength:20,
+        select:false
     }
 });
 
+userSchema.pre("save",async function(){
+    let salt =await bcrypt.genSalt(10);
+    // console.log(salt)
+    this.password=await bcrypt.hash(this.password,salt)
+});
 
+userSchema.methods.comparePasswords=async function (pass){
+    return await bcrypt.compare(pass,this.password)
+}
 
 module.exports=mongoose.model("User",userSchema)
