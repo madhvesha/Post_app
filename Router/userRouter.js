c5c88aeb6d05e8b4b34968c1a5a139b27a463dbc
Madhvesha Patil <madhveshapatil@gmail.com>
2025-11-21 13:41:00 +0530

    ninth

--- Router/userRouter.js
+++ Router/userRouter.js
@@ -1,6 +1,7 @@
 const {Router}=require('express')
 const router=Router()
 const {registerNewUser, loginUser}=require('../Controller/userController')
+
 router.post('/user',registerNewUser)
 router.post('/loginUser',loginUser)
 
